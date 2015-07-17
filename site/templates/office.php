78dd66d42693d94f74eddb1614bd4817e94b07ea
Lucas F <lfayolle1@gmail.com>
2015-07-17 15:44:55 +0200

    office page done + implemented sticky footer

--- site/templates/office.php
+++ site/templates/office.php
@@ -1,15 +1,26 @@
+<?php snippet('head') ?>
 <?php snippet('header') ?>
 
-  <main class="main" role="main">
-
-    <div class="text">
-    	<h1><?php echo $page->title()->html() ?></h1>
-	
-		<h2>This is the office.php template</h2>
-
-      	<?php echo $page->text()->kirbytext() ?>
-    </div>
-
-  </main>
+<!-- Main Content -->
+<div id="main" class="office">
+	<div class="row">
+		<div class="small-12 medium-3 large-2 pull-left">
+			<ul>
+				<li>Architecture</li>
+				<li>Interior Design</li>
+				<li>Lighting Design</li>
+				<li>Landscaping</li>
+				<li>Restoration</li>
+				<li>Urban Planning</li>
+			</ul>
+		</div>
+		<div class="small-12 medium-6 large-9 pull-right">
+			<p>For our  clients we take  responsibility  as  architects for a project  as a  whole, from a rchitecture  to  interior  design.</p>
+			<p>In  cooperation  with  our  extern  partners,  renowned  experts  in  their subject, we have acquired  high  experience in the  planning  and  execution  of  outstanding   pojects of  international  level, such  as  the  Yoo Penthouse,  the  hotel  Amano Grand Central and further major luxury living projects in Berlin and Switzerland.</p>
+			<p>Based  on this  background  we  guarantee   high  quality  standards in  architecture as  well as in cost  controlling and time management. But  first  of all and maybe even more important, we are striving to meet  the visions and  the  needs and wants of our clients.</p>
+			<p>Excellent  architecture and design as we see it is  not an end  in itself but  should first serve to  enhance  the quality of life for the people who live in the building and add value to the urban surroundings.</p>
+		</div>
+	</div>
+</div>
 
 <?php snippet('footer') ?>
