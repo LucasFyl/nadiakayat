05c91f7b1fc46582a7ec4c10d584080b056d5e10
Lucas Fayolle <lfayolle1@gmail.com>
2015-08-25 17:55:11 +0200

    adding in disabled projects

--- app/site/templates/projects.php
+++ app/site/templates/projects.php
@@ -14,6 +14,36 @@
 			<h2><a href="<?php  echo $project->url(); ?>"><?php  echo $project->title()->html(); ?></a></h2>
 	  	</div>
 	  	<?php endforeach ?>
+	  	<div class="small-12 medium-6">
+			<h2 class="disabled">Penthouse Charlottenstraße</h2>
+	  	</div>
+	  	<div class="small-12 medium-6">
+			<h2 class="disabled">Showroom für Luxusimmobilien</h2>
+	  	</div>
+	  	<div class="small-12 medium-6">
+			<h2 class="disabled">Hotel Monbijou</h2>
+	  	</div>
+	  	<div class="small-12 medium-6">
+			<h2 class="disabled">Showflat</h2>
+	  	</div>
+	  	<div class="small-12 medium-6">
+			<h2 class="disabled">Lacrem-Swisscosmetics</h2>
+	  	</div>
+	  	<div class="small-12 medium-6">
+			<h2 class="disabled">Showroom</h2>
+	  	</div>
+	  	<div class="small-12 medium-6">
+			<h2 class="disabled">Haus Märkisches Ufer</h2>
+	  	</div>
+	  	<div class="small-12 medium-6">
+			<h2 class="disabled">Hotel Ferrier</h2>
+	  	</div>
+	  	<div class="small-12 medium-6">
+			<h2 class="disabled">Kone</h2>
+	  	</div>
+	  	<div class="small-12 medium-6">
+			<h2 class="disabled">Millenium Projekt</h2>
+	  	</div>
 	</section>
 
 </div>
